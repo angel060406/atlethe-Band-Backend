3896b357901bd9ef7b07f81db20cb7e7c0e4cb27
angel060406 <angelpollo060@gmail.com>
2024-11-07 12:18:44 -0600

    [ADD] cors to frontend

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const userRoutes = require('./routes/userRoutes');
-const sensorRoutes =require('./routes/sensorRoutes');
+// const sensorRoutes =require('./routes/sensorRoutes');
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
 app.use('/', userRoutes);
-app.use('/', sensorRoutes);
+//app.use('/', sensorRoutes);
 
 module.exports = app;
