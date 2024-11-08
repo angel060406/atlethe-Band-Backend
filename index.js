268af531952f89353bc5cb417e4087a37d6ca89d
angel060406 <angelpollo060@gmail.com>
2024-11-07 18:48:55 -0600

    [ADD] ws connection & send to frontend

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const app = require('./app');
+const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer(app);
+
+// Inicializar WebSocket en el mismo servidor HTTP
+const wss = new WebSocket.Server({ server });
+
+app.set('wss', wss);
+
+wss.on('connection', (ws) => {
+    console.log('Cliente conectado a WebSocket');
+    ws.on('close', () => console.log('Cliente desconectado de WebSocket'));
+});
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`SERVER is running at port: ${PORT}`);
 });
--- utils/mqttClient.js
+++ utils/mqttClient.js
@@ -1,7 +1,7 @@
 const mqtt = require('mqtt');
+const WebSocket = require('ws');
 const { processSensorData } = require('../controllers/sensorController');
 
-// Configuración del broker MQTT
 const brokerUrl = 'mqtt://broker.emqx.io';
 const topic = 'testtopic/test';
 
@@ -9,7 +9,7 @@ const options = {
     port: 1883,
 };
 
-// Conectar al broker MQTT
+// Conexion al broker MQTT
 const client = mqtt.connect(brokerUrl, options);
 
 client.on('connect', () => {
@@ -23,22 +23,37 @@ client.on('connect', () => {
     });
 });
 
+client.on('message', (topic, message) => {
+    try {
+        const data = JSON.parse(message.toString());
+        console.log(`Mensaje recibido en ${topic}:`, data);
+
+        // Obtiene la instancia de ws
+        const wss = require('../app').get('wss');
+
+        // Extraer la temperatura corporal y enviarla al front
+        const { "temperatura del corporal": objectTemp } = data;
+        if (objectTemp !== undefined) {
+            console.log('Enviando temperatura corporal por WebSocket:', objectTemp);
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({ temperature: objectTemp }));
+                }
+            });
+        }
+
+        processSensorData(data);
+    } catch (error) {
+        console.error('Error al procesar el mensaje JSON:', error);
+    }
+});
+
 client.on('reconnect', () => {
     console.log('Reconectando al broker MQTT...');
 });
 
 client.on('error', (error) => {
     console.error('Error al conectar con el broker MQTT:', error);
-});
-
-client.on('message', (topic, message) => {
-    try {
-        const data = JSON.parse(message.toString());
-        console.log(`Mensaje recibido en ${topic}:`, data);
-        processSensorData(data);
-    } catch (error) {
-        console.error('Error al procesar el mensaje JSON:', error);
-    }
 });
 
 module.exports = client;
