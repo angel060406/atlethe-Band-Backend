4e99da00160505d3e3a8a5679138185d76c25d3e
Ubuntu <ubuntu@ip-172-31-88-89.ec2.internal>
2024-11-22 03:32:59 +0000

    Resolución de conflicto en app.js

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
+<<<<<<< Updated upstream
     //origin: 'http://localhost:5173',
     origin: 'http://34.226.61.201',
+=======
+    origin: 'https://athlete-band.integrador.xyz',
+>>>>>>> Stashed changes
     credentials: true,
 }));
 
